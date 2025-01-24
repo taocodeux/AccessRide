c25f83fc4d8ffe0223283385c5e7e260dccc39ef
Bamidele Badmus Taofikat <taofeekatb1@gmail.com>
2025-01-24 17:56:16 +0100

    signup form validation done, in progress sign in

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -2,9 +2,25 @@ import React from 'react'
 import Logo from "../accessride-images/AccessRide-logo-removebg-preview.png"
 import {Link} from "react-router-dom"
 import { useFormik} from 'formik'
+import * as yup from "yup"
 
 function SignIn() {
-  const inputStyles = "border-2 border-secondary p-2 mb-6 rounded-xl"
+  const inputStyles = "border-2 border-secondary p-2 rounded-xl"
+  const errorStyles = "text-red-600 text-sm mb-4" 
+
+  const formik = useFormik({
+    initialValues:{
+        emailAddress:"",
+        passWord:"",
+    },
+    validationSchema:yup.object({
+        emailAddress:yup.string().email("please enter a valid email").required("compulsory*"),
+        passWord:yup.string().required("compulsory*")
+    }),
+    onSubmit:(values)=>{
+        
+    }
+  })
   return (
     <>
         <div className='p-6 flex flex-col justify-center items-center m-auto w-full h-full'>
@@ -12,11 +28,13 @@ function SignIn() {
                 <img src={Logo} alt="Logo" className='border-2 border-accent lg:w-[100px] lg:h-[100px] bg-white rounded-full md:w-[100px] md:h-[100px] sm:w-[60px] sm:h-[60px]'/>
             </div>
             <h3 className='font-medium text-2xl'>Welcome back! Sign in</h3>
-            <form action="" className='flex flex-col w-1/2'>
+            <form action="" className='flex flex-col w-1/2' onSubmit={formik.handleSubmit}>
                 <label htmlFor="EmailAddress">EmailAddress:<span>*</span></label>
-                    <input type="email" id='EmailAddress' placeholder='Enter your email address'className={inputStyles}/>
+                    <input type="email" id='EmailAddress' name='emailAddress' value={formik.values.emailAddress} onChange={formik.handleChange} onBlur={formik.handleBlur} placeholder='Enter your email address'className={inputStyles}/>
+                    {formik.touched.emailAddress && formik.errors.emailAddress ?(<div className={errorStyles}>{formik.errors.emailAddress}</div>): null}
                 <label htmlFor="Password">Password:<span>*</span></label>
-                    <input type="password" id='Password' placeholder='Enter your password'className={inputStyles}/>
+                    <input type="password" id='Password' name='passWord' value={formik.values.passWord} onChange={formik.handleChange} onBlur={formik.handleBlur} placeholder='Enter your password' className={inputStyles}/>
+                    {formik.touched.passWord && formik.errors.passWord ?(<div className={errorStyles}>{formik.errors.passWord}</div>): null}
                 <button className='w-full bg-primary border-2 border-accent rounded-xl p-2 mb-6'>Sign In</button>
             </form>
             <h5>Don't have an account? 
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Logo from "../accessride-images/AccessRide-logo-removebg-preview.png"
-import { Formik, useFormik } from 'formik'
-import { Link } from 'react-router-dom'
+import { useFormik } from 'formik'
+import * as yup from "yup"
+import { Link, Navigate, useNavigate } from 'react-router-dom'
 
 function SignUp() {
-  const inputStyles = "border-2 border-secondary p-2 mb-6 rounded-xl"
+  const inputStyles = "border-2 border-secondary p-2 rounded-xl"
+  const errorStyles = "text-red-600 text-sm mb-4" 
+  const Navigate = useNavigate()
 
-  const Formik = useFormik({
+  const formik = useFormik({
     initialValues:{
       firstName:"",
       lastName:"",
@@ -15,9 +18,16 @@ function SignUp() {
       passWord:"",
     },
     onSubmit:(values)=>{
-      console.log(Formik.values)
+      localStorage.setItem("formValues", JSON.stringify(formik.values))
+      Navigate("/signin")
     },
-    
+    validationSchema:yup.object({
+      firstName:yup.string().required("compulsory*"),
+      lastName:yup.string().required("compulsory*"),
+      phoneNumber:yup.string().required("compulsory*"),
+      emailAddress:yup.string().email("please enter a valid email").required("compulsory*"),
+      passWord:yup.string().min(10, "password must be at least 10 characters").required("compulsory*"),
+    })   
   })
   return (
     <>
@@ -26,17 +36,22 @@ function SignUp() {
           <img src={Logo} alt="Logo" className='border-2 border-accent lg:w-[100px] lg:h-[100px] bg-white rounded-full md:w-[100px] md:h-[100px] sm:w-[60px] sm:h-[60px]'/>
         </div>
         <h3 className='font-medium text-2xl'>Create an account</h3>
-        <form action="" className='flex flex-col w-1/2' onSubmit={Formik.handleSubmit}>
-          <label htmlFor="firstName">First Name:<span>*</span></label>
-            <input type="text" id='firstName' name='firstName' value={Formik.values.firstName} onChange={Formik.handleChange} placeholder='Enter your first name' className={inputStyles} autoComplete='given-name'/>
-          <label htmlFor="lastName">Last Name:<span>*</span></label>
-            <input type="text" id='lastName' name='lastName' value={Formik.values.lastName} onChange={Formik.handleChange} placeholder='Enter your last name'className={inputStyles} autoComplete='family-name'/>
-          <label htmlFor="phoneNumber">Phone Number:<span>*</span></label>
-            <input type="number" id='phoneNumber' name='phoneNumber' value={Formik.values.phoneNumber} onChange={Formik.handleChange} placeholder='Enter your phone number'className={inputStyles} autoComplete='tel'/>
-          <label htmlFor="emailAddress">emailAddress:<span>*</span></label>
-            <input type="email" id='emailAddress' name='emailAddress' value={Formik.values.emailAddress} onChange={Formik.handleChange} placeholder='Enter your email address'className={inputStyles} autoComplete='email'/>
-          <label htmlFor="passWord">Password:<span>*</span></label>
-            <input type="password" id='passWord' name='passWord' value={Formik.values.passWord} onChange={Formik.handleChange} placeholder='Enter your password'className={inputStyles} autoComplete='current-password'/>
+        <form action="" className='flex flex-col w-1/2' onSubmit={formik.handleSubmit}>
+          <label htmlFor="firstName">First Name:</label>
+            <input type="text" id='firstName' name='firstName' value={formik.values.firstName} onChange={formik.handleChange} placeholder='Enter your first name' onBlur={formik.handleBlur} className={inputStyles} autoComplete='given-name'/>
+            {formik.touched.firstName && formik.errors.firstName ?(<div className={errorStyles}>{formik.errors.firstName}</div>): null}
+          <label htmlFor="lastName">Last Name:</label>
+            <input type="text" id='lastName' name='lastName' value={formik.values.lastName} onChange={formik.handleChange} placeholder='Enter your last name' onBlur={formik.handleBlur} className={inputStyles} autoComplete='family-name'/>
+            {formik.touched.lastName && formik.errors.lastName ?(<div className={errorStyles}>{formik.errors.lastName}</div>): null}
+          <label htmlFor="phoneNumber">Phone Number:</label>
+            <input type="number" id='phoneNumber' name='phoneNumber' value={formik.values.phoneNumber} onChange={formik.handleChange} placeholder='Enter your phone number' onBlur={formik.handleBlur} className={inputStyles} autoComplete='tel'/>
+            {formik.touched.phoneNumber && formik.errors.phoneNumber ?(<div className={errorStyles}>{formik.errors.phoneNumber}</div>): null}
+          <label htmlFor="emailAddress">emailAddress:</label>
+            <input type="email" id='emailAddress' name='emailAddress' value={formik.values.emailAddress} onChange={formik.handleChange} placeholder='Enter your email address' onBlur={formik.handleBlur} className={inputStyles} autoComplete='email'/>
+            {formik.touched.emailAddress && formik.errors.emailAddress ?(<div className={errorStyles}>{formik.errors.emailAddress}</div>): null}
+          <label htmlFor="passWord">Password:</label>
+            <input type="password" id='passWord' name='passWord' value={formik.values.passWord} onChange={formik.handleChange} placeholder='Enter your password' onBlur={formik.handleBlur} className={inputStyles} autoComplete='current-password'/>
+            {formik.touched.passWord && formik.errors.passWord ?(<div className={errorStyles}>{formik.errors.passWord}</div>): null}
           <button type='submit' className='w-full bg-primary border-2 border-accent rounded-xl p-2 mb-6'>Sign Up</button>
         </form>
         <h5>Already have an account? 
